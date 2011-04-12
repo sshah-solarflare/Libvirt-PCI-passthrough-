e38e98115161091a657d88476c76b352315aa394
Michal Privoznik <mprivozn@redhat.com>
2011-04-12 16:58:48 +0200

    Fix possible infinite loop in remote driver
    When we take out completed calls from queue we might end up
    in circular pointer. We don't want pointer to previous item
    point to element taken out.
    (cherry picked from commit 50e4b9195d2d8b46969940336b44221b500a2de3)
    
    Signed-off-by: Steve Hodgson <shodgson@solarflare.com>

--- src/remote/remote_driver.c
+++ src/remote/remote_driver.c
@@ -10316,8 +10316,9 @@ remoteIOEventLoop(virConnectPtr conn,
                  */
                 DEBUG("Waking up sleep %d %p %p", tmp->proc_nr, tmp, priv->waitDispatch);
                 virCondSignal(&tmp->cond);
+            } else {
+                prev = tmp;
             }
-            prev = tmp;
             tmp = tmp->next;
         }
 

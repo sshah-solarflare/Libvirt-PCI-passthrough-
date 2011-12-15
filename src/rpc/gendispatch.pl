ce98f018218824fdfa0eb6560781a0ff32a949e8
Shradha Shah <sshah@solarflare.com>
2011-12-15 13:50:22 +0000

    (none)
    Signed-off-by: Shradha Shah <sshah@solarflare.com>

--- src/rpc/gendispatch.pl
+++ src/rpc/gendispatch.pl
@@ -1452,6 +1452,8 @@ elsif ($opt_k) {
         if ($call->{streamflag} ne "none") {
             print "        virNetClientRemoveStream(priv->client, netst);\n";
             print "        virNetClientStreamFree(netst);\n";
+            print "        st->driver = NULL;\n";
+            print "        st->privateData = NULL;\n";
         }
 
         print "        goto done;\n";
